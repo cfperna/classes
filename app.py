a6297fbb9dfdabc16f8a0a132a3c82e8f53f63c7
Chris Perna <cfperna@gmail.com>
2024-02-07 14:09:06 -0800

    custum obbjects

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ class TagCloud:
         self.tags = {}
 
     def add(self, tag):
-        self.tags[tag] = self.tags.get(tag, 0)+1
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 10)+1
+
+    def __getitem__(self, tag):
+        return self.tags.get(tag.lower(), 0)
 
 
 cloud = TagCloud()
-cloud.add("python")
+cloud.add("Python")
 cloud.add("python")
 cloud.add("python")
 cloud.add("horse")
+cloud.add("horse")
+cloud.add("1")
 print(cloud.tags)
+print(cloud.__getitem__('python'))
