0e828100ae5c0c26f47dc10f85ee1df9879c8fa8
Chris Perna <cfperna@gmail.com>
2024-02-19 14:32:09 -0800

    adding inheritane stream example

--- app.py
+++ app.py
@@ -21,3 +21,37 @@ m.eat()
 print(m.age)
 print(isinstance(m, object))
 print(issubclass(Mammel, Animal))
+
+
+class RaiseOperationError(Exception):
+    pass
+
+
+class Stream:
+    def __init__(self) -> None:
+        self.opened = False
+
+    def open(self):
+        if self.opened:
+            raise RaiseOperationError('File is already open')
+        self.open = True
+
+    def close(self):
+        if not self.open:
+            raise RaiseOperationError('File is already closed')
+        self.open = False
+
+
+class FileStream(Stream):
+    def Read(self):
+        print('Reading for file')
+
+
+class NetworkStream(Stream):
+    def Read(self):
+        print('Reading data for Network')
+
+
+n = NetworkStream()
+
+n.Read()
