15e36eac9735bb7361a9a9d499b3d65a57f39394
Chris Perna <cfperna@gmail.com>
2024-02-19 11:12:50 -0800

    issubclass k

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-class Product:
-    def __init__(self, price):
-        self.price = price
+class Animal:
+    def __init__(self) -> None:
+        self.age = 1
 
-    @property
-    def price(self):
-        return self.__price
-
-    @price.setter
-    def price(self, value):
-        if value < 0:
-            raise ValueError('price cannot be negative')
-        self.__price = value
+    def eat(self):
+        print("eat")
 
 
-product = Product(10)
-print(product.price)
-product2 = Product(20)
-print(product2.price)
+class Mammel(Animal):
+    def walk(self):
+        print("walk")
 
+
+class fish(Animal):
+    def swim(self):
+        print("Swim)")
+
+
+m = Mammel()
+m.eat()
+print(m.age)
+print(isinstance(m, object))
+print(issubclass(Mammel, Animal))
