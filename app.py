3613d2f03a5cfc78c167104424b1418d04b01ac9
Chris Perna <cfperna@gmail.com>
2024-02-19 22:44:20 -0800

    Abact Base Class

--- app.py
+++ app.py
@@ -1,26 +1,3 @@
-class Animal:
-    def __init__(self) -> None:
-        self.age = 1
-
-    def eat(self):
-        print("eat")
-
-
-class Mammel(Animal):
-    def walk(self):
-        print("walk")
-
-
-class fish(Animal):
-    def swim(self):
-        print("Swim)")
-
-
-m = Mammel()
-m.eat()
-print(m.age)
-print(isinstance(m, object))
-print(issubclass(Mammel, Animal))
 
 
 class RaiseOperationError(Exception):
