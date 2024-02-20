5e8365ee4c94de3e796a51d38a12c31f6256648d
Chris Perna <cfperna@gmail.com>
2024-02-20 12:32:56 -0800

    super eample

--- app.py
+++ app.py
@@ -3,5 +3,15 @@ class Text(str):
         return self + self
 
 
+class TrackableChanges(list):
+    def append(self, object):
+        print('Append Called')
+        super().append(object)
+
+
+list = TrackableChanges()
+list.append("2")
+
+
 m = Text("Python")
 print(m.duplicate())
